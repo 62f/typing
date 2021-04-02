3423292edc691c81bdad434ac3fa68139ebd3bfe
62f <68332197+62f@users.noreply.github.com>
2021-04-02 04:16:39 -0400

    Update mod_generics_cache.py

--- python2/mod_generics_cache.py
+++ python2/mod_generics_cache.py
@@ -1,4 +1,4 @@
-"""Module for testing the behavior of generics across different modules."""
+"""Module for testing behavior of generics across different modules."""
 
 from typing import TypeVar, Generic
 
